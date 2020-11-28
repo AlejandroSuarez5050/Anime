cfc785c4aa10319d8d3144a6e9455506661c1aa2
AlejandroSuarez5050 <73505479+AlejandroSuarez5050@users.noreply.github.com>
2020-11-28 12:57:54 -0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,107 @@
+$("#btn1").click(function(){
+    swal({
+        title: "Felicidades!",
+        text: "Tu descarga ha comenzado",
+        icon: "success",
+        button: "Aww si uwu",
+      });
+});
+$("#btn2").click(function(){
+    swal({
+        title: "Felicidades!",
+        text: "Tu descarga ha comenzado",
+        icon: "success",
+        button: "Aww si uwu",
+      });
+});
+$("#btn3").click(function(){
+    swal({
+        title: "Felicidades!",
+        text: "Tu descarga ha comenzado",
+        icon: "success",
+        button: "Aww si uwu",
+      });
+});
+$("#btn4").click(function(){
+    swal({
+        title: "Felicidades!",
+        text: "Tu descarga ha comenzado",
+        icon: "success",
+        button: "Aww si uwu",
+      });
+});
+$("#btn5").click(function(){
+    swal({
+        title: "Felicidades!",
+        text: "Tu descarga ha comenzado",
+        icon: "success",
+        button: "Aww si uwu",
+      });
+});
+$("#btn6").click(function(){
+    swal({
+        title: "Felicidades!",
+        text: "Tu descarga ha comenzado",
+        icon: "success",
+        button: "Aww si uwu",
+      });
+});
+$("#btn7").click(function(){
+    swal({
+        title: "Felicidades!",
+        text: "Tu descarga ha comenzado",
+        icon: "success",
+        button: "Aww si uwu",
+      });
+});
+$("#btn8").click(function(){
+    swal({
+        title: "Felicidades!",
+        text: "Tu descarga ha comenzado",
+        icon: "success",
+        button: "Aww si uwu",
+      });
+});
+$("#btn9").click(function(){
+  swal({
+      title: "Felicidades!",
+      text: "Tu descarga ha comenzado",
+      icon: "success",
+      button: "Aww si uwu",
+    });
+});
+$("#btn10").click(function(){
+  swal({
+      title: "Felicidades!",
+      text: "Tu descarga ha comenzado",
+      icon: "success",
+      button: "Aww si uwu",
+    });
+});
+$("#btn11").click(function(){
+  swal({
+      title: "Felicidades!",
+      text: "Tu descarga ha comenzado",
+      icon: "success",
+      button: "Aww si uwu",
+    });
+});
+$("#btn12").click(function(){
+  swal({
+      title: "Felicidades!",
+      text: "Tu descarga ha comenzado",
+      icon: "success",
+      button: "Aww si uwu",
+    });
+});
+$("#btnForm").click(function(){
+  location.href = "../../index.html"; 
+});
+$("#btnModal").click(function(){
+  swal({
+      title: "Sesion Iniciada",
+      text: "Bienvenido de nuevo",
+      icon: "success",
+      button: "Aceptar",
+    });
+});
